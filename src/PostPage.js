72ac21ff0198416b4a902fb762b29fa4049e62be
lyonsja <lyonsja@oregonstate.edu>
2020-03-03 16:10:44 -0800

    More styling changes for postpage.

--- src/PostPage.js
+++ src/PostPage.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
 import React from 'react';
+import PulseBubble from './Loaders/PulseBubble';
 
 import ListingParser from './ListingParser.js';
 import SubredditSidebar from './SubredditSidebar.js';
@@ -11,31 +12,15 @@ import SubredditSidebar from './SubredditSidebar.js';
 function SubredditPage(props) {
     const { subreddit, post } = useParams();
 
-    const [open, setOpen] = useState(false);
-    const [subredditPageData, setSubredditPageData] = useState({ data: { children: [] } });
+    const [loadingPost, setLoadingPost] = useState(true);
+    const [postPageData, setPostPageData] = useState({ data: { children: [] } });
 
     // eslint-disable-next-line
     const [cookies, setCookie, removeCookie] = useCookies();
     const styling = css`
-    ${'' /* border: 1px solid red; */}
-
-    
     position: relative;
-
-    .subreddit-stretch {
-      ${'' /* border: 1px solid red; */}
-
-      width: 100%;
-      height: auto;
-      height: 91vh;
-      max-width: 15%;
-      flex-shrink: 10;
-
-      border-right: ${open ? "1px solid rgb(180, 180, 180)" : "1px solid rgba(180, 180, 180, 0)"};
-      transition: ${open ? "border 0.8s ease-in-out 0.2s" : "border 0.6s ease-in-out"};
-    }
     .comment-box{
-        padding: 10px;
+        padding: 15px;
         height: auto;
         border: 1px solid black;
         margin-bottom: 10px;
@@ -48,11 +33,17 @@ function SubredditPage(props) {
         min-width: 600px;
         margin-bottom: 20px;
         display: block;
-        height: 100px;
+        height: auto;
         background-color: lightgrey;
         color: black;
         line-height: 20px;
-        padding: 10px;
+        padding: 15px;
+        margin-left: 20px;
+        margin-right: 20px;
+    }
+    .post-title{
+        font-size: 22px;
+        margin-bottom: 10px;
     }
     .comments-area{
         display: flex;
@@ -60,12 +51,18 @@ function SubredditPage(props) {
         align-items: flex-start;
         justify-content: flex-end;
     }
+    .bubble-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      }
   `;
 
     useEffect(() => {
         async function fetchSubredditPage() {
             let responseBody = {};
-            // setLoadingUser(true);
+            setLoadingPost(true);
             const response = await fetch(
                 ("https://oauth.reddit.com/r/" + subreddit + "/comments/" + post + "?raw_json=1&?sort=confidence"),
                 {
@@ -80,16 +77,15 @@ function SubredditPage(props) {
             responseBody = await response.json();
             console.log(responseBody);
 
-            setSubredditPageData(responseBody)
-            // setLoadingUser(false)
+            setPostPageData(responseBody)
+            setLoadingPost(false)
         }
         fetchSubredditPage()
-    }, [subreddit, cookies.accessToken, cookies.username, cookies.redditApp, cookies.redditVersion]);
+    }, [subreddit, post, cookies.accessToken, cookies.username, cookies.redditApp, cookies.redditVersion]);
 
-
-    function commentParser() {
+    function simpleCommentParser() {
         try {
-            var comments = subredditPageData[1].data.children.map(comment =>
+            var comments = postPageData[1].data.children.map(comment =>
                 <div className="comment-box" key={comment.data.id}>{comment.data.body}</div>
             );
             return comments;
@@ -102,12 +98,12 @@ function SubredditPage(props) {
     function postParser() {
         //TODO: handle text vs image posts
         try {
-            var selftext = subredditPageData[0].data.children[0].data.selftext;
-            var title = subredditPageData[0].data.children[0].data.title;
+            var selftext = postPageData[0].data.children[0].data.selftext;
+            var title = postPageData[0].data.children[0].data.title;
             console.log("selftext is: " + selftext + " title is: " + title);
             return (
                 <>
-                    <div>
+                    <div className="post-title">
                         {title}
                     </div>
                     <div>
@@ -123,15 +119,25 @@ function SubredditPage(props) {
     }
 
     var postText = postParser();
-    var commentElements = commentParser();
+    var commentElements = simpleCommentParser();
 
 
     return (
+
         <div css={styling}>
-            <div className="post-container">{postText}</div>
-            <div className="comments-area">{commentElements}</div>
-            <div className="subreddit-stretch"></div>
-            <SubredditSidebar open={open} setOpen={setOpen} />
+
+            {loadingPost ? (
+                <div className="bubble-container">
+                    <PulseBubble />
+                </div>
+            ) :
+                <div>
+                    <div className="post-container">{postText}</div>
+                    <div>Comments:</div>
+                    <div className="comments-area">{commentElements}</div>
+                    <div className="subreddit-stretch"></div>
+                </div>
+            }
         </div>
     );
 }
